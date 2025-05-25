c9e298a93370354ffdb204e258c3c5f30a5e90d7
HiKris1801 <k.komissarova1801@gmail.com>
2025-05-25 09:05:06 -0400

    update main.py

--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -3,8 +3,13 @@ from gendiff.modules.parser import parser_json
 
 
 def main():
-    cli()
-    parser_json()
+    arg = cli()
+    filepath1 = arg.first_file
+    filepath2 = arg.second_file
+    parser_json(filepath1)
+    parser_json(filepath2)
+    print(parser_json(filepath1))
+    print(parser_json(filepath2))
 
 
 if __name__ == "__main__":
