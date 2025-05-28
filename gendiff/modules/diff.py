8b99d2a49dd5d444830f12a07f54871037e57196
HiKris1801 <k.komissarova1801@gmail.com>
2025-05-28 11:49:22 -0400

    add draft diff.py

--- gendiff/modules/diff.py
+++ gendiff/modules/diff.py
@@ -1,2 +1,27 @@
-def generate_diff(file_path1, file_path2):
-    
+from gendiff.modules.parser import parser_json
+
+def generate_diff(filepath1, filepath2):
+    #result_diff=" "
+    #dict1 = parser_json(filepath1)
+    #dict2 = parser_json(filepath2)
+    #if dict1 != dict2:
+    #    for key in sorted(set(dict1.keys()) | set(dict2.keys())):
+    #        if key in dict1 and key in dict2:
+    #            result_diff[key] += f"  {dict1[key]}"
+    #        if key in dict1:
+    #            result_diff[key]  +=f"-  {dict1[key]}"
+    #        elif key not in dict1 and key in dict2:
+    #            result_diff[key] += f"+ {dict2[key]}"
+    #        elif key not in dict1 and key in dict1:
+    #            result_diff[key] += f"+  {dict2[key]}"
+
+    #    return result_diff
+
+#diff = generate_diff(filepath1, filepath2)
+#print(diff)
+
+
+
+
+
+
--- gendiff/scripts/main.py
+++ gendiff/scripts/main.py
@@ -1,5 +1,6 @@
 from gendiff.cli import cli
 from gendiff.modules.parser import parser_json
+#from gendiff.modules.diff import generate_diff
 
 
 def main():
@@ -8,8 +9,9 @@ def main():
     filepath2 = arg.second_file
     parser_json(filepath1)
     parser_json(filepath2)
-    print(parser_json(filepath1))
-    print(parser_json(filepath2))
+    #generate_diff(filepath1, filepath2)
+    #print(generate_diff())
+    #print(parser_json(filepath2))
 
 
 if __name__ == "__main__":
